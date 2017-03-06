10a78fe0e4fd6cd72134b1d05ae9d804a9ae4fda
C0D1NGD0J0 <web-dev@United4Life.local>
2017-03-06 18:50:38 +0100

    added error checking code

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ request({
 		url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 		json: true
 	}, (err, res, body) => {
-		console.log(`Address: ${body.results[0].address_components[3].short_name}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`,`Longitude: ${body.results[0].geometry.location.lng}` );
+	if(err){
+		console.log('Unable to connect to Google server');
+	} else if(body.status === 'ZERO_RESULTS') {
+		console.log('Unable to find the provided address');
+	} else if(body.status === 'OK'){
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
 });
