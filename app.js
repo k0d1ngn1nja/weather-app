151cc570c3957d8586a084e1e8be1d23a54f8919
C0D1NGD0J0 <web-dev@United4Life.local>
2017-03-06 18:37:11 +0100

    fetching user input from the cli

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+	address: {
+		demand: true,
+		alias: 'a',
+		describe: 'address to fetch weather',
+		string: true
+	} 
+}).help().alias('help', 'h').argv;
+
+let encodedAddress = encodeURIComponent(argv.address);
 
 request({
-		url: 'http://maps.googleapis.com/maps/api/geocode/json?address=40%20hill%20street%20london',
+		url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 		json: true
 	}, (err, res, body) => {
-		// console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Address: ${body.results[0].address_components[3].short_name}`);
 		console.log(`Latitude: ${body.results[0].geometry.location.lat}`,`Longitude: ${body.results[0].geometry.location.lng}` );
 });
