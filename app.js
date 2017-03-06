2a0150518af34ad9ca9d4e0cf8d7d01267fd5812
C0D1NGD0J0 <web-dev@United4Life.local>
2017-03-06 18:12:36 +0100

    retrieve location latitude/longitude from googlemap server

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ request({
 		url: 'http://maps.googleapis.com/maps/api/geocode/json?address=40%20hill%20street%20london',
 		json: true
 	}, (err, res, body) => {
-		console.log(JSON.stringify(body, undefined, 2));
+		// console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`,`Longitude: ${body.results[0].geometry.location.lng}` );
 });
