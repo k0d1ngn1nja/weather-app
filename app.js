17a3fc642f445456df4f1db60fd19a7bcf346400
C0D1NGD0J0 <web-dev@United4Life.local>
2017-03-06 17:23:10 +0100

    requesting address info from google map api

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require('request');
+
+request({
+		url: 'http://maps.googleapis.com/maps/api/geocode/json?address=40%20hill%20street%20london',
+		json: true
+	}, (err, res, body) => {
+		console.log(body);
+});
